1890eeb673dff3c63e269d8fa521fa6738fbd59c
steven.jardine@gmail.com <steven.jardine@gmail.com@8949352d-533d-b96e-911a-2b62ddc9d241>
2012-07-11 15:41:05 +0000

    Added authenticated user information to the GwtSecurityException.

--- branches/requestfactory/core/src/main/java/com/gwt/ss/client/exceptions/GwtSecurityException.java
+++ branches/requestfactory/core/src/main/java/com/gwt/ss/client/exceptions/GwtSecurityException.java
@@ -8,7 +8,11 @@ import com.google.gwt.user.client.rpc.IsSerializable;
  */
 public class GwtSecurityException extends RuntimeException implements IsSerializable {
 
-    private static final long serialVersionUID = 986114451704820985L;
+    private static final long serialVersionUID = 6376681305631679931L;
+
+    private boolean authenticated = false;
+
+    private String authenticatedUser = null;
 
     public GwtSecurityException() {
         super();
@@ -24,6 +28,34 @@ public class GwtSecurityException extends RuntimeException implements IsSerializ
 
     public GwtSecurityException(Throwable cause) {
         super(cause);
+    }
+
+    /**
+     * @return the authenticatedUser
+     */
+    public String getAuthenticatedUser() {
+        return authenticatedUser;
+    }
+
+    /**
+     * @return the authenticated
+     */
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    /**
+     * @param authenticated the authenticated to set
+     */
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+    /**
+     * @param authenticatedUser the authenticatedUser to set
+     */
+    public void setAuthenticatedUser(String authenticatedUser) {
+        this.authenticatedUser = authenticatedUser;
     }
 
 }
