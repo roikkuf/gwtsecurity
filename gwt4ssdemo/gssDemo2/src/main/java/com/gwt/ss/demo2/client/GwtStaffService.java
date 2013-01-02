b68d9c71cb6e1bcaeb24acdb355c29b3d21d1bab
steven.jardine <steven.jardine@gmail.com>
2013-01-02 21:15:13 +0000

    [maven-release-plugin]  copy for tag gwtsecurity-1.2.0

--- gwt4ssdemo/gssDemo2/src/main/java/com/gwt/ss/demo2/client/GwtStaffService.java
+++
@@ -1,7 +0,0 @@
-package com.gwt.ss.demo2.client;
-
-import com.gwt.ss.sharedservice.client.Remote;
-
-public interface GwtStaffService extends Remote {
-
-}
--- gwt4ssdemo/gssDemo3/src/main/java/com/gwt/ss/demo3/server/ApplicationContext.java
+++
@@ -1,9 +0,0 @@
-package com.gwt.ss.demo3.server;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:applicationContext.xml")
-public class ApplicationContext {
-}
