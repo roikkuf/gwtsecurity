f196a0fa5ba156ccc42864eab5b4b280f97a0371
steven.jardine@gmail.com <steven.jardine@gmail.com@8949352d-533d-b96e-911a-2b62ddc9d241>
2013-04-19 15:16:42 +0000

    [maven-release-plugin]  copy for tag gwtsecurity-1.2.1

---
+++ tags/gwtsecurity-1.2.1/core/src/main/java/com/gwt/ss/client/exceptions/GwtSessionAuthenticationException.java
@@ -0,0 +1,27 @@
+package com.gwt.ss.client.exceptions;
+
+/**
+ * Denote Server side occur {@link org.springframework.security.web.authentication.session.SessionAuthenticationException SessionAuthenticationException}<br/>
+ * 表示主機端發生{@link org.springframework.security.web.authentication.session.SessionAuthenticationException 會議驗證異常}
+ */
+public class GwtSessionAuthenticationException extends GwtAuthenticationException {
+
+    private static final long serialVersionUID = -4721682851766662179L;
+
+    public GwtSessionAuthenticationException() {
+        super();
+    }
+
+    public GwtSessionAuthenticationException(String message) {
+        super(message);
+    }
+
+    public GwtSessionAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GwtSessionAuthenticationException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ tags/gwtsecurity-1.2.1/core/src/main/java/com/gwt/ss/shared/GwtConst.java
@@ -0,0 +1,23 @@
+/**
+ * $Id$
+ * Copyright (c) 2012 MJN Services, Inc., All Rights Reserved.
+ */
+package com.gwt.ss.shared;
+
+/**
+ * @version $Rev$
+ * @author Steven Jardine
+ */
+public final class GwtConst {
+
+    public static final String GWT_RPC_CONTENT_TYPE = "text/x-gwt-rpc";
+
+    public static final String GWT_RF_HEADER = "x-gwtsecurity-rf-request";
+
+    public static final String SPRING_SECURITY_LAST_USERNAME_KEY = "SPRING_SECURITY_LAST_USERNAME";
+
+    private GwtConst() {
+        super();
+    }
+
+}
