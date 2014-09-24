bedbb12d575136dec199982b0c2086375bac4c7d
steven.jardine <steven.jardine@gmail.com>
2014-09-24 16:39:55 +0000

    [maven-release-plugin]  copy for tag gwtsecurity-1.3.1

--- gwt4ssdemo/gssDemo3/src/main/java/com/gwt/ss/demo3/client/StaffService.java
+++
@@ -1,9 +0,0 @@
-package com.gwt.ss.demo3.client;
-
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-import com.gwt.ss.sharedservice.client.Remote;
-
-@RemoteServiceRelativePath("../sgs/staff")
-public interface StaffService extends Remote{
-
-}
